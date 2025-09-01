084485f15854088e0e66bbda65e585614b5f9d6e
sam j <jlatha541@gmail.com>
2025-09-01 07:28:32 +0530

    script.js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 // Simple JS placeholder - can be expanded for animations or form handling
 console.log("Zephoria '25 website loaded");
+
+const location = {
+  name: "Vel Tech High Tech College, Chennai",
+  latitude: 13.1877832,
+  longitude: 80.1064227,
+  mapsUrl: "https://www.google.com/maps/place/Vel+Tech+High+Tech+Dr.Rangarajan+Dr.Sakunthala+Engineering+College/@13.1877832,80.1064227,17z"
+};
+
+console.log(`College: ${location.name}`);
+console.log(`Map: ${location.mapsUrl}`);
